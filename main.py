835883af8b753f6c14a2b17ca954e8bd359daf3c
floriandc <66038044+floriandc@users.noreply.github.com>
2022-12-15 15:18:30 +0100

    demander prix et quantité et calculer total

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
 def HelloWorld():
     return("Hello world")
 
+def get_price():
+    # On demande à l'utilisateur de saisir le prix unitaire de l'article
+    price = input("Veuillez saisir le prix unitaire de l'article : ")
+    
+    # On convertit la valeur saisie en nombre à virgule flottante
+    price = float(price)
+    
+    # On retourne le prix
+    return price
+
+def get_quantity():
+    # On demande à l'utilisateur de saisir la quantité d'articles souhaitée
+    quantity = input("Veuillez saisir la quantité d'articles souhaitée : ")
+    
+    # On convertit la valeur saisie en entier
+    quantity = int(quantity)
+    
+    # On retourne la quantité
+    return quantity
+
+def get_total_price(price, quantity):
+    # On calcule le prix total en multipliant le prix unitaire par la quantité
+    total_price = price * quantity
+    
+    # On retourne le prix total
+    return total_price
--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 import unittest
 from main import HelloWorld
+from main import get_total_price
 
-class TestHelloWorld(unittest.TestCase):
+class test(unittest.TestCase):
     def test_hello_world(self):
         # On vérifie que la fonction "hello_world" retourne bien la chaîne de caractères "Hello world"
         self.assertEqual(HelloWorld(), "Hello world")
-        print("1")
+
+    def test_get_total_price(self):
+        # On vérifie que la fonction "get_total_price" retourne bien le prix total en multipliant le prix unitaire par la quantité
+        self.assertEqual(get_total_price(10, 5), 50)
+        self.assertEqual(get_total_price(20, 3), 60)
+        self.assertEqual(get_total_price(100, 1), 100)
+
+
 
 if __name__ == "__main__":
     unittest.main()
